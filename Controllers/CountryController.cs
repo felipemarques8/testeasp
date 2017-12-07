6b1e9cd3c9ffdcfd604b8d53eb27998783c76d86
Felipe Marques <lipemarques8@gmail.com>
2017-12-07 16:59:33 -0300

    first commit

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngularJSDemo.Controllers
+{
+    public class CountryController : ApiController
+    {
+        // GET api/country
+        public IEnumerable<System.Web.Mvc.SelectListItem> Get()
+        {
+            List<System.Web.Mvc.SelectListItem> countries = new List<System.Web.Mvc.SelectListItem>
+            {
+                new System.Web.Mvc.SelectListItem { Text = "Brasil", Value="BR" },
+            };
+
+            return countries;
+        }
+
+        // GET api/country/5
+        public IEnumerable<System.Web.Mvc.SelectListItem> Get(string id)
+        {
+            List<System.Web.Mvc.SelectListItem> states = new List<System.Web.Mvc.SelectListItem>();
+
+            switch (id)
+            {
+                
+                case "BR":
+                    states.Add(new System.Web.Mvc.SelectListItem { Text = "Pernambuco", Value = "PE" });
+                    states.Add(new System.Web.Mvc.SelectListItem { Text = "São Paulo", Value = "SP" });
+                    break;
+                
+            }
+
+            return states;
+        }
+    }
+}
